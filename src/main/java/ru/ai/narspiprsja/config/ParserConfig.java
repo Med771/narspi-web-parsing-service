96cabe6e615f9b0e4d2e1148df55fc6cce60bd0b
Titov Ilya <titov1608@gmail.com>
2025-11-26 13:56:40 +0300

    optmz ParserTools

--- src/main/java/ru/ai/narspiprsja/config/ParserConfig.java
+++ src/main/java/ru/ai/narspiprsja/config/ParserConfig.java
@@ -1,20 +1,26 @@
 package ru.ai.narspiprsja.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.format.DateTimeFormatter;
 
 @Configuration
+@ConfigurationProperties(prefix = "parser")
+@Data
 public class ParserConfig {
-    public static final String BASE_URL = "https://%s.cap.ru/press_center/news";
-    public static final String NEWS_PATH = "?date_start=%s&page=%s";
+
+    private String baseUrl;
+    private String newsPath;
+    private String userAgent;
+    private String referrer;
+    private String acceptLanguage;
+    private int timeout;
+    private int sleep;
+
+    private int retryCount = 3;
+    private long retryDelay = 500;
+
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm | dd.MM.yyyy");
-
-    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
-            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36";
-
-    public static final String REFERRER = "https://www.google.com";
-    public static final String ACCEPT_LANGUAGE = "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7";
-    public static final int TIMEOUT_MS = 10_000;
-    public static final int SLEEP_MS = 100;
 }
--- src/main/java/ru/ai/narspiprsja/property/ParserProperty.java
+++ src/main/java/ru/ai/narspiprsja/property/ParserProperty.java
@@ -4,7 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @Component
@@ -16,4 +18,6 @@ public class ParserProperty {
     private String splitter;
     private int maxTokens;
     private int overlapTokens;
+
+    private Map<String, String> selectors = new HashMap<>();
 }
